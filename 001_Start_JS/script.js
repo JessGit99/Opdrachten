119ec23545ba07a097609cb1adddd50c21c1afad
JessGit99 <84376340+JessGit99@users.noreply.github.com>
2021-06-23 13:38:10 +0200

    Add files via upload

---
+++ 001_Start_JS/script.js
@@ -0,0 +1,23 @@
+// Commentaar
+
+console.log("Hello Winc Academy");
+
+let naam = 'Jessica';
+console.log(naam);
+
+console.log("==================");
+let som = 4 / 4;
+console.log(som);
+
+console.log("==================");
+let stringSom = "4" + "4";
+console.log(stringSom);
+
+console.log("==================");
+let leeftijd = 45;
+console.log("Leeftijd: " + typeof leeftijd);
+
+console.log("==================");
+let leeftijdTekst = "Vijfenveertig";
+console.log("LeeftijdTekst: " + typeof leeftijdTekst);
+
