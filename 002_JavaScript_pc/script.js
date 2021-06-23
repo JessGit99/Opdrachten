fa053a3f12dfc84db843027645f1dae746f88312
JessGit99 <84376340+JessGit99@users.noreply.github.com>
2021-06-23 17:30:14 +0200

    Add files via upload

---
+++ 002_JavaScript_pc/script.js
@@ -0,0 +1,9 @@
+console.log("Hallo Winc Academy studenten");
+
+// Dit is een grote som
+
+/* console.log("=== Uitkomst 1230941 x 11823794 ===");
+console.log(1230941 * 1823794);
+
+console.log("=== Uitkomst 637263 / 54 ===");
+console.log(637263 / 54); */
