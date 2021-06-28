3fa2c3c393eede858d72584c050bf2638cebb340
JessGit99 <84376340+JessGit99@users.noreply.github.com>
2021-06-28 17:27:40 +0200

    Add files via upload

---
+++ 003_Kroegbaas_if_else/script.js
@@ -0,0 +1,39 @@
+const age = 18;
+const isFemale = false;
+const driverStatus = "Kees";
+
+console.log("========================================================================");
+if(age >= 18) {
+
+    if (isFemale) {
+        console.log("U mag naar binnen, u bent 18+ en vrouw.");
+    }
+
+    else {
+        console.log("U mag niet naar binnen, u bent 18+ maar geen vrouw.");
+
+        }
+    }
+
+    else {
+        console.log("U mag niet naar binnen, u bent geen 18+.");
+    }
+
+console.log("");
+
+if(driverStatus == "Bob") {
+    console.log("U bent Bob, u mag geen alcohol drinken, maar u mag wel rijden.");
+    }
+    else {
+        console.log("U ben niet de Bob, dus u mag wel alcohol drinken, maar niet rijden.");
+        }
+console.log("========================================================================");
+console.log("");
+
+
+
+
+
+
+
+
