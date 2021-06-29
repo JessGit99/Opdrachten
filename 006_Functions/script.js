c9edc3b35a54b954e951024f05f5f54de3530f86
JessGit99 <84376340+JessGit99@users.noreply.github.com>
2021-06-29 14:37:03 +0200

    Add files via upload

---
+++ 006_Functions/script.js
@@ -0,0 +1,37 @@
+const makePizza = function() {
+    console.log("");
+    console.log("===== Recept pizza =====");
+    console.log("1. Verwarm de oven voor op 200 graden.");
+    console.log("2. Maak de ingredienten voor op de pizza klaar.");
+    console.log("3. Leg de pizzabodem op de bakplaat.");
+    console.log("4. Verdeel de saus over de pizzabodem.");
+    console.log("5. Verdeel de ingredienten over de pizza.");
+    console.log("6. Zet de pizza in de oven.");
+    console.log("7. Haal na 15 minuten de pizza uit de oven.");
+    console.log("8. Verdeel halve tomaatjes en basilicumblaadjes over de pizza.");
+    console.log("9. Aanvallen maar!");
+    console.log("");
+}
+
+const makeSushi = function () {
+    console.log("");
+    console.log("===== Recept sushi =====");
+    console.log("1. Kook de rijst.");
+    console.log("2. Maak de ingredienten klaar voor gebruik.");
+    console.log("3. Rooster sesamzaad ongeveer 5 minuten in een koekepan.");
+    console.log("4. Verdeel rijst over een vel en bestrooi met sesamzaad.");
+    console.log("5. Leg de overige ingredienten op de rijst.");
+    console.log("6. Rol het vel op.");
+    console.log("7. Snijd de rol in kleine stukjes van een paar centimeter.");
+    console.log("8. Aanvallen maar!");
+    console.log("");
+}
+
+makeSushi();
+makeSushi();
+makeSushi();
+
+makePizza();
+makePizza();
+
+
