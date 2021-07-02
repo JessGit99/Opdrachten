ae5699e46bfedafa8649267000174230c015eb20
JessGit99 <84376340+JessGit99@users.noreply.github.com>
2021-07-02 09:06:33 +0200

    Add files via upload

---
+++ 011_While_For_Loops/multiplication-tables.js
@@ -0,0 +1,11 @@
+// for( let i = 1; i <= 10;i++) {
+//     console.log(i + " x 9 = " + (i * 9));
+// }
+
+
+for(let i = 1; i <= 10; i++) {
+    console.log("====" + " TAFEL VAN " + i + "====");
+    for(let j = 1; j <=10; j++) {
+        console.log(j + " x " + i + " = " + (i * j));
+    }
+}
